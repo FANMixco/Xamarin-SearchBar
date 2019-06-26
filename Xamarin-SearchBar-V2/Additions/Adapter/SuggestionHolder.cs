0be55b52258b77797d425580ac97f24e2eabeb76
Federico Navarrete <fanmixco@gmail.com>
2019-06-26 20:09:18 +0200

    First version.

---
+++ Xamarin-SearchBar-V2/Additions/Adapter/SuggestionHolder.cs
@@ -0,0 +1,12 @@
+﻿using Android.Support.V7.Widget;
+using Android.Views;
+
+namespace Com.Mancj.Materialsearchbar.Adapter
+{
+    public class SuggestionHolder : RecyclerView.ViewHolder
+    {
+        public SuggestionHolder(View itemView) : base(itemView)
+        {
+        }
+    }
+}
