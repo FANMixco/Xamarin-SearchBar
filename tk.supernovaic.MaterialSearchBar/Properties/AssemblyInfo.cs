9c52d90a8fe7172a129b5ae0fbd2ae18a03d21ff
Federico Navarrete <darklord.navarrete@gmail.com>
2020-07-15 07:47:17 +0200

    Assembly validations

--- tk.supernovaic.MaterialSearchBar/Properties/AssemblyInfo.cs
+++ tk.supernovaic.MaterialSearchBar/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using Android.App;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("tk.supernovaic.MaterialSearchBar")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Material Design Search Bar for Xamarin")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("tk.supernovaic.MaterialSearchBar")]
@@ -26,5 +26,5 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.3")]
+[assembly: AssemblyFileVersion("1.2.0.3")]
